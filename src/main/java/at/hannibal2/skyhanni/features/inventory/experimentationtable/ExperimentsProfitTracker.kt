4f2670359dbdbfcb2442a5ff2c784decaa039647
Helium9 <80259183+KeinsWolfi@users.noreply.github.com>
2025-03-31 22:28:38 +0200

    Update ExperimentsProfitTracker.kt

--- src/main/java/at/hannibal2/skyhanni/features/inventory/experimentationtable/ExperimentsProfitTracker.kt
+++ src/main/java/at/hannibal2/skyhanni/features/inventory/experimentationtable/ExperimentsProfitTracker.kt
@@ -133,7 +133,7 @@ object ExperimentsProfitTracker {
         blockedReason = when {
             enchantingExpPattern.matches(reward) && ExperimentMessages.EXPERIENCE.isSelected() -> "EXPERIENCE_DROP"
             experienceBottleChatPattern.matches(reward) && ExperimentMessages.BOTTLES.isSelected() -> "BOTTLE_DROP"
-            listOf("Metaphysical Serum", "Experiment The Fish").contains(reward) && ExperimentMessages.MISC.isSelected() -> "MISC_DROP"
+            listOf("Metaphysical Serum", "Experiment The Fish", "Guardian").contains(reward) && ExperimentMessages.MISC.isSelected() -> "MISC_DROP"
             ExperimentMessages.ENCHANTMENTS.isSelected() -> "ENCHANT_DROP"
             else -> ""
         }
