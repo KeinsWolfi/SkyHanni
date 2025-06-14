2436b24343c12cda69b665a86d65fd23adc3e69c
Helium9 <80259183+KeinsWolfi@users.noreply.github.com>
2025-06-14 22:09:56 +0200

    macrohandler

--- src/main/java/at/hannibal2/skyhanni/features/webhook/MacroHandler.kt
+++ src/main/java/at/hannibal2/skyhanni/features/webhook/MacroHandler.kt
@@ -2,20 +2,43 @@ package at.hannibal2.skyhanni.features.webhook
 
 import at.hannibal2.skyhanni.SkyHanniMod
 import at.hannibal2.skyhanni.api.event.HandleEvent
-import at.hannibal2.skyhanni.events.chat.SkyHanniChatEvent
+import at.hannibal2.skyhanni.config.commands.CommandCategory
+import at.hannibal2.skyhanni.config.commands.CommandRegistrationEvent
+import at.hannibal2.skyhanni.events.minecraft.packet.PacketSentEvent
 import at.hannibal2.skyhanni.skyhannimodule.SkyHanniModule
-import at.hannibal2.skyhanni.utils.ChatUtils
+import at.hannibal2.skyhanni.utils.ScreenshotUtil
+import net.minecraft.network.play.client.C01PacketChatMessage
 
 @SkyHanniModule
 object MacroHandler {
     private val config get() = SkyHanniMod.feature.webhook
 
     @HandleEvent
-    fun onChat(event: SkyHanniChatEvent) {
-        val msg = event.message
+    fun onSendMessageToServerPacket(event: PacketSentEvent) {
+        val packet = event.packet as? C01PacketChatMessage ?: return
 
-        if (msg.startsWith("ASDIUAIUSUDZ")) {
-            ChatUtils.chat("§c§lMacroHandler: §r§cThis is a test message, please ignore it.")
+        val message = packet.message
+        if (!config.macroWebhooks) return
+        if (message.startsWith("macrowebhookbegin")) {
+            val screenShot = ScreenshotUtil.captureScreenshot()
+            Webhook().addEmbed(
+                DiscordEmbed(
+                    title = "Macro Started: ${message.removePrefix("macrowebhookbegin").trim()}",
+                    image = EmbedImage("attachment://${screenShot.fileName}"),
+                )
+            ).sendWebhookWithFile(
+                file = screenShot.path.toFile(),
+            )
+        } else if (message.startsWith("macrowebhookend")) {
+            val screenShot = ScreenshotUtil.captureScreenshot()
+            Webhook().addEmbed(
+                DiscordEmbed(
+                    title = "Macro Ended: ${message.removePrefix("macrowebhookend").trim()}",
+                    image = EmbedImage("attachment://${screenShot.fileName}"),
+                )
+            ).sendWebhookWithFile(
+                file = screenShot.path.toFile()
+            )
         }
     }
 }
